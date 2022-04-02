1ea6a25f0823695866c7806ee9c0accb188d4eb3
jon-michael-c <jonmichaelcubano@gmail.com>
2022-04-02 12:09:07 -0400

    updated

--- src/assets/components/Sidebar/index.js
+++ src/assets/components/Sidebar/index.js
@@ -1,7 +1,10 @@
 import "./index.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import LogoS from "../../images/logo-s.png";
 import LogoSubtitle from "../../images/logo_sub.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -9,6 +12,47 @@ const Sidebar = () => (
       <img src={LogoS} />
       <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
     </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4de" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4de" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4de" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="nonreferrer"
+          href="https://www.linkedin.com/in/jon-michael-cubano-4a1316178"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="nonreferrer"
+          href="https://github.com/jon-michael-c"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
