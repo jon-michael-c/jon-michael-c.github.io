63f247b9fb3ed443d6a34d5ef3388f3240352038
jon-michael-c <jonmichaelcubano@gmail.com>
2022-04-03 21:12:06 -0400

    added rounte home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import "./App.scss";
-import { Routes, Route } from "react-router-dom";
-import Layout from "./assets/components/Layout";
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './assets/components/Layout'
+import Home from './assets/components/Home'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
